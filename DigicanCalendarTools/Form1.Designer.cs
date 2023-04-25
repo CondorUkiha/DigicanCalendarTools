4aab1a7c9c629deec181b0f42c8f448c7713f2b5
CondorUkiha <kon.09270hrk@gmail.com>
2023-04-25 14:53:56 +0900

    完成

--- DigicanCalendarTools/Form1.Designer.cs
+++ DigicanCalendarTools/Form1.Designer.cs
@@ -31,7 +31,12 @@
             this.openFileButton = new System.Windows.Forms.Button();
             this.openFile = new System.Windows.Forms.Label();
             this.openFileLabel = new System.Windows.Forms.Label();
+            this.runButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SaveFilelabel = new System.Windows.Forms.Label();
+            this.SaveFile = new System.Windows.Forms.Label();
             this.saveFileButton = new System.Windows.Forms.Button();
+            this.DataGetSelect = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // openFileButton
@@ -66,22 +71,79 @@
             this.openFileLabel.Text = "デジキャン出欠状況確認ファイル";
             this.openFileLabel.Click += new System.EventHandler(this.label1_Click_1);
             // 
+            // runButton
+            // 
+            this.runButton.Location = new System.Drawing.Point(344, 340);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(85, 31);
+            this.runButton.TabIndex = 5;
+            this.runButton.Text = "実行";
+            this.runButton.UseVisualStyleBackColor = true;
+            this.runButton.Click += new System.EventHandler(this.saveFileButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(83, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 23);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "読み込み授業選択";
+            this.label1.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // SaveFilelabel
+            // 
+            this.SaveFilelabel.AutoSize = true;
+            this.SaveFilelabel.Location = new System.Drawing.Point(83, 224);
+            this.SaveFilelabel.Name = "SaveFilelabel";
+            this.SaveFilelabel.Size = new System.Drawing.Size(149, 18);
+            this.SaveFilelabel.TabIndex = 10;
+            this.SaveFilelabel.Text = "出力ファイル保存先";
+            // 
+            // SaveFile
+            // 
+            this.SaveFile.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.SaveFile.Location = new System.Drawing.Point(86, 247);
+            this.SaveFile.Margin = new System.Windows.Forms.Padding(10, 5, 10, 5);
+            this.SaveFile.Name = "SaveFile";
+            this.SaveFile.Padding = new System.Windows.Forms.Padding(5);
+            this.SaveFile.Size = new System.Drawing.Size(500, 30);
+            this.SaveFile.TabIndex = 9;
+            this.SaveFile.Text = "保存先が選択されていません";
+            // 
             // saveFileButton
             // 
-            this.saveFileButton.Location = new System.Drawing.Point(358, 305);
+            this.saveFileButton.Location = new System.Drawing.Point(599, 246);
             this.saveFileButton.Name = "saveFileButton";
             this.saveFileButton.Size = new System.Drawing.Size(85, 31);
-            this.saveFileButton.TabIndex = 5;
-            this.saveFileButton.Text = "実行";
+            this.saveFileButton.TabIndex = 8;
+            this.saveFileButton.Text = "選択";
             this.saveFileButton.UseVisualStyleBackColor = true;
-            this.saveFileButton.Click += new System.EventHandler(this.saveFileButton_Click);
+            this.saveFileButton.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // DataGetSelect
+            // 
+            this.DataGetSelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.DataGetSelect.FormattingEnabled = true;
+            this.DataGetSelect.Items.AddRange(new object[] {
+            "全ての授業",
+            "開始時間が回っていない授業",
+            "終了時間が回っていない授業"});
+            this.DataGetSelect.Location = new System.Drawing.Point(86, 181);
+            this.DataGetSelect.Name = "DataGetSelect";
+            this.DataGetSelect.Size = new System.Drawing.Size(598, 26);
+            this.DataGetSelect.TabIndex = 11;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DataGetSelect);
+            this.Controls.Add(this.SaveFilelabel);
+            this.Controls.Add(this.SaveFile);
             this.Controls.Add(this.saveFileButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.runButton);
             this.Controls.Add(this.openFileLabel);
             this.Controls.Add(this.openFile);
             this.Controls.Add(this.openFileButton);
@@ -97,7 +159,12 @@
         private System.Windows.Forms.Label openFile;
         private System.Windows.Forms.Label openFileLabel;
         private System.Windows.Forms.Button openFileButton;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label SaveFilelabel;
+        private System.Windows.Forms.Label SaveFile;
         private System.Windows.Forms.Button saveFileButton;
+        private System.Windows.Forms.ComboBox DataGetSelect;
     }
 }
 
